e568311b2ad48a8e4781db7bc1871154d2a36637
soeyzandiego@gmail.com <59988129+soeyzandiego@users.noreply.github.com>
2025-11-06 17:41:45 -0500

    breathing in and out detection

--- Assets/Scripts/BreathReader.cs
+++ Assets/Scripts/BreathReader.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System.Collections;
 using System.Security.Policy;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 using System.Collections.Generic;
 using System.IO.Ports;
@@ -9,19 +10,25 @@ using TMPro;
 
 public class BreathReader : MonoBehaviour
 {
-    bool breathe_in = false; // false = breathe out
+    bool breatheIn = false; // false = breathe out
     float sensorADC = 0; // the value returned by the analog port the stretch sensor is plugged into
+    float lastReading;
 
     float adcMin = 10000;
     float adcMax = 0;
 
     [SerializeField] TMP_Text adcText;
     [SerializeField] TMP_Text avgText;
+    [SerializeField] TMP_Text minMaxText;
+    [SerializeField] GameObject calibratingText;
+    [SerializeField] Image bg;
+    Color red;
 
     [Space(10)]
     [SerializeField] KeyCode startSampleKey = KeyCode.Space;
     bool sampling = true;
     bool calibrating = false;
+    bool calibrated = false;
 
     [SerializeField] UnityEvent onBreatheIn;
     [SerializeField] UnityEvent onBreatheOut;
@@ -35,7 +42,7 @@ public class BreathReader : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        red = bg.color;
     }
 
     // Update is called once per frame
@@ -48,6 +55,14 @@ public class BreathReader : MonoBehaviour
         if (Input.GetKeyDown(startSampleKey))
         {
             calibrating = !calibrating;
+
+            calibratingText.SetActive(calibrating);
+
+            if (!calibrating)
+            {
+                minMaxText.text = "Min, Max: " + adcMin.ToString() + ", " + adcMax.ToString();
+                calibrated = true;
+            }
         }
 
         if (calibrating)
@@ -56,7 +71,17 @@ public class BreathReader : MonoBehaviour
             if (sensorADC <adcMin) { adcMin = sensorADC; }
         }
 
-        if (sampling)
+        adcText.text = "ADC: " + sensorADC.ToString();   
+
+        if (breatheIn) { bg.color = Color.green; }
+        else { bg.color = red; }
+    }
+
+    public void OnMessageArrived(string msg)
+    {
+        sensorADC = float.Parse(msg);
+
+        if (calibrated)
         {
             if (adcAverage.Count >= sampleFrames) { adcAverage.Dequeue(); }
             adcAverage.Enqueue(sensorADC);
@@ -66,19 +91,17 @@ public class BreathReader : MonoBehaviour
             float currentSample = Mathf.Floor(total / adcAverage.Count);
 
             if (samples.Count > sampleFrames) { samples.Dequeue(); }
-            samples.Enqueue(sensorADC);
-            //lastSample = currentSample;
+            samples.Enqueue(currentSample);
 
-            adcText.text = "ADC: " + sensorADC.ToString();
+            breatheIn = currentSample > lastSample;
+            lastSample = currentSample;
+            breatheIn = sensorADC > samples.Peek();
+            Debug.Log(currentSample + ", " + samples.Peek().ToString());
+
             avgText.text = "ADC Avg: " + currentSample.ToString();
         }
 
-        
-    }
-
-    public void OnMessageArrived(string msg)
-    {
-        sensorADC = float.Parse(msg);
+        lastReading = sensorADC;
     }
 
     void OnConnectionEvent(bool success)
