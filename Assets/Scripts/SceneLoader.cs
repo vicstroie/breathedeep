4857d6c2cafb7e936c3b6e5929075469cdd03c9c
soeyzandiego@gmail.com <59988129+soeyzandiego@users.noreply.github.com>
2025-12-04 23:44:31 -0500

    main menu started, reorganized scripts folder

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] Image blackout;
+
+    [SerializeField] float fadeSpeed = 0.25f;
+
+    public static SceneLoader instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (instance == null) { instance = this; DontDestroyOnLoad(gameObject); }
+        else { Destroy(gameObject); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeOutAndLoad(int index)
+    {
+        StartCoroutine(FadeOut(index));
+    }
+
+    IEnumerator FadeOut(int index)
+    {
+        float a = 0;
+        Color c = blackout.color;
+        blackout.color = new Color(c.r, c.g, c.b, a);
+
+        while (a < 1)
+        {
+            a += fadeSpeed * Time.deltaTime;
+            blackout.color = new Color(c.r, c.g, c.b, a);
+            yield return null;
+        }
+        blackout.color = new Color(c.r, c.g, c.b, 1);
+        SceneManager.LoadScene(index);
+
+        while (a > 0)
+        {
+            a -= fadeSpeed * Time.deltaTime;
+            blackout.color = new Color(c.r, c.g, c.b, a);
+            yield return null;
+        }
+        blackout.color = new Color(c.r, c.g, c.b, 0);
+    }
+}
